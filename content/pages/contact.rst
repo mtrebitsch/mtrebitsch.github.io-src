1256162ab959e5cfc72713ff33dd723c895325f8
Maxime Trebitsch <maxime.trebitsch@ens-lyon.org>
2018-10-11 18:35:32 +0200

    Huh?

---
+++ content/pages/contact.rst
@@ -0,0 +1,22 @@
+Contact infos
+#############
+
+:slug: contact
+:title: Contact
+
+If you want to get in touch with me, the easiest way is by email.
+
+Email
+    * maxime.trebitsch .at. iap.fr
+    * maxime.trebitsch .at. ens-lyon.org
+Address
+    | Institut d'Astrophysique de Paris,
+    | 98bis boulevard Arago,
+    | 75014 Paris,
+    | France
+
+..
+   Tel
+       +33 (0)4 78 86 83 88
+
+You can also find me on Twitter (`@maximetrebitsch <https://twitter.com/maximetrebitsch>`_) where I try to occasionally post stuff, sometimes even related to science!
---
+++ pelicanconf.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Maxime Trebitsch'
+SITENAME = u'Maxime Trebitsch'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+TAGS_SAVE_AS = ''
+TAG_SAVE_AS = ''
+DISPLAY_TAGS_ON_SIDEBAR = False
+
+
+# # Pages settings
+PAGE_URL = '{slug}.html'
+PAGE_SAVE_AS = '{slug}.html'
+PAGE_LANG_URL = '{slug}-{lang}.html'
+PAGE_LANG_SAVE_AS = '{slug}-{lang}.html'
+
+# Menu
+DISPLAY_PAGES_ON_MENU = False
+DISPLAY_CATEGORIES_ON_MENU = False
+MENUITEMS = (
+    ('Research', 'research.html'),
+    ('CV', 'cv.html'),
+    ('Contact', 'contact.html'),
+)
+
+AVATAR = "img/maximetrebitsch.jpg"
+ABOUT_ME = "I'm a post-doctoral researcher in astrophysics working within the BLACK project at the IAP in Paris, interested in high-redshift galaxy formation."
+
+
+# # Blogroll
+# LINKS = (('Pelican', 'http://getpelican.com/'),
+#          ('Python.org', 'http://python.org/'),
+#          ('Jinja2', 'http://jinja.pocoo.org/'),
+#          ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('Twitter', 'https://twitter.com/maximetrebitsch'),
+          ('Bitbucket', 'https://bitbucket.org/mtrebitsch'),
+          ('GitHub', 'https://github.com/mtrebitsch'),
+          ('ResearchGate', 'https://www.researchgate.net/profile/Maxime_Trebitsch'),
+)
+DEFAULT_PAGINATION = 5
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+THEME = './pelican-themes/pelican-bootstrap3'
+BOOTSTRAP_THEME = 'cosmo'
+
+# Theme stuff
+JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
+
+PLUGIN_PATHS = ['./pelican-plugins']
+PLUGINS = ['render_math',
+           'i18n_subsites',   ## For Bootstrap
+           'twitter_bootstrap_rst_directives',
+           'liquid_tags.img', 'liquid_tags.video',
+           'liquid_tags.youtube', 'liquid_tags.vimeo',
+           'liquid_tags.include_code', # 'liquid_tags.notebook'
+]
+
+MATH_JAX = {'mathjax_font': 'sanserif'}
+
+
+STATIC_PATHS = ['img', 'pdf', 'video']
+TYPOGRIFY = True
+TWITTER_USERNAME = 'maximetrebitsch'
